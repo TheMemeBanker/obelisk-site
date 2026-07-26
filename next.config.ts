e9237a56b59e3a62b78c2679f20599eca78476e0
TheMemeBanker <thememebanker@users.noreply.github.com>
2026-07-26 10:19:46 -0500

    attach custom domain obelisk.fund

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", basePath: "/obelisk-site", assetPrefix: "/obelisk-site/", images: { unoptimized: true }, trailingSlash: true,
+  output: "export", images: { unoptimized: true }, trailingSlash: true,
   /* config options here */
 };
 
